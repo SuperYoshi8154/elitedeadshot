6c70fa5df9cad8b732fb1357ae38b19b1e550f5c
SuperYoshi8154 <njww200819@gmail.com>
2026-09-13 01:28:34 -0500

    hi

--- src/main.js
+++ src/main.js
@@ -2,21 +2,28 @@ import './style.css'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
+<section id="header">
+  <div>
+    <h1>Welcome to EliteDeadshot</h1>
+  </div>
+</section>
+
 <section id="info">
   <div>
     <h1>Hi I'm Nora</h1>
-    <p>this is what I am working on</p>
+    <p>My name is Nora and I am a web developer</p>
   </div>
 </section>
 
 <section id="links">
   <ul>
-    <li><a href="https://github.com/SuperYoshi8154" target="_blank">GitHub</a></li>
     <li><a href="https://elitedeadshot.app/" target="_blank">Will be something</a></li>
   </ul>
 </section>
 
-<section id="spacer"></section>
+<section id="spacer">
+  <li><a href="https://github.com/SuperYoshi8154" target="_blank">GitHub</a></li>
+</section>
 `
 
 setupCounter(document.querySelector('#counter'))
